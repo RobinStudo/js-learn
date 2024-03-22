d88f7dd166e466d78357df6111976375561242ef
Robin Delbaere <hello@rdelbaere.fr>
2024-03-22 10:04:06 +0100

    TS setup

--- project-3/frontend/webpack.config.js
+++ project-3/frontend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/app.js",
+    entry: "./src/app.ts",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist'),
