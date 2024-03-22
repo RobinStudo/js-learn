e9ef51946a0eb54d29a1e148e76dda37793ea8fd
Robin Delbaere <hello@rdelbaere.fr>
2024-03-22 09:52:44 +0100

    Webpack env

---
+++ project-3/frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+};
